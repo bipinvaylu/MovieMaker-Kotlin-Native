84b22114aedc5c195629b84a2dbe9067c3361dc7
Bipin Vayalu <bipinvaylu@gmail.com>
2018-07-05 04:06:28 +0530

    framework removed

---
+++ ios-app/MovieMaker/MovieMaker/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MovieMaker
+//
+//  Created by Bipin Vaylu on 05/07/18.
+//  Copyright © 2018 Bipin Vaylu. All rights reserved.
+//
+
+import UIKit
+import KotlinMovieMaker
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
