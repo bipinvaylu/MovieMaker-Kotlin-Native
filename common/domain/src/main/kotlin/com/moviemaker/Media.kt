abf8ea58d4dac4e1d16652a11cab34e17238d62d
Bipin Vayalu <bipinvaylu@gmail.com>
2018-07-07 02:51:23 +0530

    Added media class

--- common/domain/src/main/kotlin/com/moviemaker/Media.kt
+++ common/domain/src/main/kotlin/com/moviemaker/Media.kt
@@ -15,4 +15,70 @@ sealed class Media {
     abstract val createdDate: Long
     abstract val fileSize: Long
 
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // Functions
+
+
+    abstract fun getRelatedPaths(): Collection<String>
+
+    abstract fun withPath(path: String): Media
+
+    abstract fun withThmPath(thmPath: String): Media
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // Instances
+
+    data class Video(
+        override val path: String,
+        override val thmPath: String,
+        override val createdDate: Long,
+        override val fileSize: Long,
+        val duration: Long,
+        val lrvPath: String,
+        val csvPath: String
+    ) : Media() {
+
+        override fun getRelatedPaths(): Collection<String> {
+            return listOf(lrvPath, csvPath, thmPath)
+        }
+
+        override fun withPath(path: String): Media {
+            return copy(path = path)
+        }
+
+        override fun withThmPath(thmPath: String): Media {
+            return copy(thmPath = thmPath)
+        }
+
+    }
+
+    data class Image(
+        override val path: String,
+        override val thmPath: String,
+        override val createdDate: Long,
+        override val fileSize: Long
+    ) : Media() {
+
+        override fun getRelatedPaths(): Collection<String> {
+            return listOf(thmPath)
+        }
+
+        override fun withPath(path: String): Media {
+            return copy(path = path)
+        }
+
+        override fun withThmPath(thmPath: String): Media {
+            return copy(thmPath = thmPath)
+        }
+
+    }
+
+    companion object {
+
+        val VIDEO_EXTENSIONS = arrayOf("MP4")
+        val IMAGE_EXTENSIONS = arrayOf("JPG")
+
+    }
+
 }
