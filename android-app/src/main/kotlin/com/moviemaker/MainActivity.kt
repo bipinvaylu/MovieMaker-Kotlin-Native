641f25908adb34b0cec99a0b82086b10bc91302e
Bipin Vayalu <bipinvaylu@gmail.com>
2018-07-09 23:10:53 +0530

    Fixed compilation issues

--- android-app/src/main/kotlin/com/moviemaker/MainActivity.kt
+++ android-app/src/main/kotlin/com/moviemaker/MainActivity.kt
@@ -3,6 +3,7 @@ package com.moviemaker
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.TextView
+import com.moviemaker.domain.Media
 import java.util.Date
 
 class MainActivity : AppCompatActivity() {
--- common/domain/src/main/kotlin/com/moviemaker/domain/Media.kt
+++ common/domain/src/main/kotlin/com/moviemaker/domain/Media.kt
@@ -1,4 +1,4 @@
-package com.moviemaker
+package com.moviemaker.domain
 
 sealed class Media {
 
