531ce41c5b2533f38a9b7971d87071526ea609ba
Bipin Vayalu <bipinvaylu@gmail.com>
2018-07-11 02:34:34 +0530

    Updated gradle to use dependecy

--- android-app/src/test/kotlin/com/moviemaker/datasource/LocalMediaDataSourceImpl.kt
+++
@@ -1,9 +0,0 @@
-package com.moviemaker.datasource
-
-import com.moviemaker.domain.Media
-
-class LocalMediaDataSourceImpl: LocalMediaDataSource() {
-    override fun getMediaList(onComplete: (List<Media>) -> Unit) {
-        //TODO: Read from shared preferences
-    }
-}
