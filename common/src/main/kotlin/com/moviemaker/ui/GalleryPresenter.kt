0fee7fd9dd1d933da947d1e71f53d6013c00fc66
Bipin Vayalu <bipinvaylu@gmail.com>
2018-07-11 22:47:36 +0530

    Merged domain, app, view modules into common due to various compilation issues

--- common/src/main/kotlin/com/moviemaker/ui/GalleryPresenter.kt
+++ common/src/main/kotlin/com/moviemaker/ui/GalleryPresenter.kt
@@ -1,9 +1,10 @@
 package com.moviemaker.ui
 
 import com.moviemaker.base.BasePresenter
+import com.moviemaker.interactor.GetMediaList
 
 class GalleryPresenter(
-
+private val getMediaList: GetMediaList
 ): BasePresenter<GalleryView>() {
 
     override fun onViewAttached() {
