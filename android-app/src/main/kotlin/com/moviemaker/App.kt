8554aa1f5c21a46ca2418d1191153b0efeaa18db
Bipin Vayalu <bipinvaylu@gmail.com>
2018-07-16 19:55:16 +0530

    Added Picasso for loading images

--- android-app/src/main/kotlin/com/moviemaker/App.kt
+++ android-app/src/main/kotlin/com/moviemaker/App.kt
@@ -3,6 +3,7 @@ package com.moviemaker
 import android.app.Application
 import com.moviemaker.settings.SettingsRepository
 import com.moviemaker.utils.settingsRepository
+import com.squareup.picasso.Picasso
 import timber.log.Timber
 
 class App : Application() {
@@ -23,6 +24,10 @@ class App : Application() {
         val settingsRepo: SettingsRepository by lazy {
             Timber.d("Bipin - settingsRepo init called")
             settingsRepository(instance)
+        }
+
+        val picasso: Picasso by lazy {
+            Picasso.Builder(instance).build()
         }
     }
 }
--- android-app/src/main/kotlin/com/moviemaker/ui/media/MediaItemView.kt
+++ android-app/src/main/kotlin/com/moviemaker/ui/media/MediaItemView.kt
@@ -1,15 +1,12 @@
 package com.moviemaker.ui.media
 
 import android.content.Context
-import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.net.Uri
-import android.provider.MediaStore
 import android.util.AttributeSet
 import android.widget.FrameLayout
 import android.widget.ImageView
 import com.airbnb.epoxy.EpoxyModel
+import com.moviemaker.App
 import com.moviemaker.R
 import com.moviemaker.domain.Media
 import kotterknife.bindView
@@ -27,32 +24,24 @@ class MediaItemView : FrameLayout {
     @JvmOverloads
     @Suppress("ConvertSecondaryConstructorToPrimary")
     constructor(
-        context: Context,
-        attrs: AttributeSet? = null,
-        defStyleAttr: Int = 0
+            context: Context,
+            attrs: AttributeSet? = null,
+            defStyleAttr: Int = 0
     ) : super(context, attrs, defStyleAttr)
 
 
     // public functions
     fun bind(media: Media) {
         val imageUri = Uri.parse(media.path)
-        context.grantUriPermission(context.packageName, imageUri,
-            Intent.FLAG_GRANT_READ_URI_PERMISSION)
-        val bitmap = MediaStore.Images.Media.getBitmap(context.contentResolver, imageUri)
-//        val bitmap = getBitmapFromUri(imageUri)
-        imageView.setImageBitmap(bitmap)
-    }
-
-    private fun getBitmapFromUri(uri: Uri): Bitmap {
-        val parcelFileDescriptor = context.contentResolver.openFileDescriptor(uri, "r")
-        val image = BitmapFactory.decodeFileDescriptor(parcelFileDescriptor.fileDescriptor)
-        parcelFileDescriptor.close()
-        return image
+        App.picasso
+                .load(imageUri)
+                .placeholder(R.drawable.ic_media_24px)
+                .into(imageView)
     }
 
     // Model
     data class Model(
-        val media: Media
+            val media: Media
     ) : EpoxyModel<MediaItemView>() {
 
         private var mediaItemView: MediaItemView? = null
