a9f3ac9ac7e807a5163621899329c2dc025c31be
Bipin Vayalu <bipinvaylu@gmail.com>
2018-07-18 21:04:07 +0530

    Assinged imageview width/height

--- android-app/src/main/kotlin/com/moviemaker/ui/media/MediaItemView.kt
+++ android-app/src/main/kotlin/com/moviemaker/ui/media/MediaItemView.kt
@@ -1,5 +1,6 @@
 package com.moviemaker.ui.media
 
+import android.content.ContentUris
 import android.content.Context
 import android.net.Uri
 import android.provider.MediaStore
@@ -11,12 +12,8 @@ import com.moviemaker.App
 import com.moviemaker.R
 import com.moviemaker.domain.Media
 import com.moviemaker.extension.isImageUri
-import io.reactivex.Observable
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
 import kotterknife.bindView
 import timber.log.Timber
-import java.io.File
 
 
 class MediaItemView : FrameLayout {
@@ -44,35 +41,44 @@ class MediaItemView : FrameLayout {
             App.component.picasso()
                     .load(mediaUri)
                     .into(imageView)
+            Timber.d("Bipin - ImageView width: ${imageView.width}, height: ${imageView.height}")
         } else {
-            Observable.create<String> {emitter->
-                val projection = arrayOf(MediaStore.Video.Media.DATA)
-                val videoId = mediaUri.toString().split("/").last()
-                Timber.d("Bipin - videoId: $videoId")
-                val cursor = context.contentResolver.query(
-                        MediaStore.Video.Thumbnails.EXTERNAL_CONTENT_URI,
-                        projection,
-                        MediaStore.Video.Thumbnails.VIDEO_ID + "=?",
-                        arrayOf(videoId),
-                        null
-                )
-                cursor.moveToFirst()
-                val columnIndex = cursor.getColumnIndex(projection[0])
-                val thumbnailPath = cursor.getString(columnIndex)
-                cursor.close()
-                Timber.d("Bipin - thumbnailPath: $thumbnailPath")
-//                val bitmap = ThumbnailUtils.createVideoThumbnail(thumbnailPath, MediaStore.Video.Thumbnails.FULL_SCREEN_KIND)
-//                emitter.onNext(bitmap)
-                emitter.onNext(thumbnailPath)
-                emitter.onComplete()
-            }.observeOn(AndroidSchedulers.mainThread())
-                    .subscribeOn(Schedulers.io())
-                    .doOnNext { thumbnailPath ->
-                        App.component.picasso()
-                                .load(File(thumbnailPath))
-                                .into(imageView)
-                    }
-                    .subscribe()
+            val videoId = mediaUri.toString().split("/").last().toLong()
+            val width = App.component.context().resources.getDimensionPixelSize(R.dimen.material_increment_9x)
+            val height = App.component.context().resources.getDimensionPixelSize(R.dimen.material_increment_5x)
+            App.component.picasso().load(
+                    ContentUris.withAppendedId(
+                            MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+                            videoId
+                    )
+            )
+                    .resize(width, height)
+                    .centerCrop()
+                    .into(imageView)
+//            Observable.create<String> {emitter->
+//                val projection = arrayOf(MediaStore.Video.Media.DATA)
+//                val videoId = mediaUri.toString().split("/").last()
+//                val cursor = context.contentResolver.query(
+//                        MediaStore.Video.Thumbnails.EXTERNAL_CONTENT_URI,
+//                        projection,
+//                        MediaStore.Video.Thumbnails.VIDEO_ID + "=?",
+//                        arrayOf(videoId),
+//                        null
+//                )
+//                cursor.moveToFirst()
+//                val columnIndex = cursor.getColumnIndex(projection[0])
+//                val thumbnailPath = cursor.getString(columnIndex)
+//                cursor.close()
+//                emitter.onNext(thumbnailPath)
+//                emitter.onComplete()
+//            }.observeOn(AndroidSchedulers.mainThread())
+//                    .subscribeOn(Schedulers.io())
+//                    .doOnNext { thumbnailPath ->
+//                        App.component.picasso()
+//                                .load(File(thumbnailPath))
+//                                .into(imageView)
+//                    }
+//                    .subscribe()
         }
     }
 
