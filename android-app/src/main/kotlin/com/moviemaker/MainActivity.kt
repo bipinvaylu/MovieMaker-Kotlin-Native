08682738ad029dce6b4b3278280aeb6af62faf49
Bipin Vayalu <bipinvaylu@gmail.com>
2018-07-19 20:23:59 +0530

    code cleanup

--- android-app/src/main/kotlin/com/moviemaker/MainActivity.kt
+++ android-app/src/main/kotlin/com/moviemaker/MainActivity.kt
@@ -9,7 +9,6 @@ import android.provider.MediaStore
 import android.support.design.widget.FloatingActionButton
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
-import android.view.View
 import com.moviemaker.domain.Media
 import com.moviemaker.extension.isImageUri
 import com.moviemaker.extension.showMediaChooser
@@ -34,8 +33,9 @@ class MainActivity : AppCompatActivity() {
     private val mediaGridView: MediaGridView by bindView(R.id.media_grid_view)
     private val addImageButton: FloatingActionButton by bindView(R.id.add_image_button)
     private val toolbar: Toolbar by bindView(R.id.toolbar)
-    private val mainView: View by bindView(R.id.media_grid_view)
 
+
+    // Override functions
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -46,43 +46,12 @@ class MainActivity : AppCompatActivity() {
             showMediaChooser(IMAGE_CHOOSER_REQUEST_CODE)
         }
 
-
-
         rxPermissions.request(READ_PERMISSION, WRITE_PERMISSION)
                 .filter { true }
                 .subscribe {
                     mediaGridView.loadMediaList()
                 }
-//        if (ContextCompat.checkSelfPermission(this, READ_PERMISSION) != PackageManager.PERMISSION_GRANTED &&
-//                ContextCompat.checkSelfPermission(this, WRITE_PERMISSION) != PackageManager.PERMISSION_GRANTED) {
-//            ActivityCompat.requestPermissions(
-//                    this,
-//                    arrayOf(READ_PERMISSION, WRITE_PERMISSION),
-//                    READ_PERMISSION_REQUEST_CODE
-//            )
-//        } else {
-//            mediaGridView.loadMediaList()
-//        }
-
     }
-
-//    override fun onRequestPermissionsResult(requestCode: Int,
-//                                            permissions: Array<String>,
-//                                            grantResults: IntArray
-//    ) {
-//        when (requestCode) {
-//            READ_PERMISSION_REQUEST_CODE,
-//            WRITE_PERMISSION_REQUEST_CODE -> {
-//                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-//                    mediaGridView.loadMediaList()
-//                } else {
-//                    // permission denied, boo! Disable the
-//                    // functionality that depends on this permission.
-//                }
-//                return
-//            }
-//        }
-//    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -94,7 +63,7 @@ class MainActivity : AppCompatActivity() {
                 val selected: List<Uri> = Matisse.obtainResult(data)
                 Timber.d("selected: $selected")
                 selected.forEach {
-                    val media = retriveMediaDetail(it)
+                    val media = retrieveMediaDetail(it)
                     media?.let {
                         emitter.onNext(it)
                     }
@@ -111,7 +80,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun retriveMediaDetail(uri: Uri): Media? {
+    private fun retrieveMediaDetail(uri: Uri): Media? {
         val isVideo = !uri.isImageUri()
 
         val mediaColumns = arrayListOf(
@@ -140,67 +109,7 @@ class MainActivity : AppCompatActivity() {
         cursor.close()
         return Media(uri.toString(), createdDate, fileSize, duration)
     }
-
-//    private fun getMedia(uri: Uri): Media {
-//        return if (uri.toString().contains("images")) {
-//            Media.Image(
-//                    uri.toString(),
-//                    Date().time,
-//                    0L
-//            )
-//        } else {
-//            Media.Video(
-//                    uri.toString(),
-//                    Date().time,
-//                    0L
-//            )
-//        }
-//    }
-
-//    private fun getImageDetails(imageUri: Uri, fileDirectory: File): Media {
-//
-//        val filePathColumn = arrayOf(MediaStore.Images.Media.DATA)
-//
-//        // Get the cursor
-//        val cursor = contentResolver.query(imageUri,
-//                filePathColumn, null, null, null)
-//        // Move to first row
-//        cursor.moveToFirst()
-//
-//        val columnIndex = cursor.getColumnIndex(filePathColumn[0])
-//        val imgDecodableString = cursor.getString(columnIndex)
-//
-//        val fileGallery = File(imgDecodableString)
-//        val bitmap = BitmapFactory.decodeFile(fileGallery.absolutePath)
-//
-//        cursor.close()
-//
-//        val imageName = UUID.randomUUID().toString() + ".PNG"
-//
-//        val baos = ByteArrayOutputStream()
-//        bitmap.compress(Bitmap.CompressFormat.PNG, 50, baos)
-//        val b = baos.toByteArray()
-//
-//        val file = File(fileDirectory, imageName)
-//
-//        try {
-//            val out = FileOutputStream(file)
-//            out.write(b)
-//            out.flush()
-//            out.close()
-//        } catch (e: Exception) {
-//            e.printStackTrace()
-//        }
-//
-//
-//        val uri = Uri.fromFile(file)
-//        val fileSize = file.inputStream().available()
-//        return Media.Image(
-//                uri.toString(),
-//                Date().time,
-//                fileSize.toLong()
-//        )
-//    }
+    
 
     companion object {
 
